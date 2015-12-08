99d7c31166a47c8d36543c2a36ef72195c295048
jeffward01 <jeffwardcoding@gmail.com>
2015-12-08 10:32:43 -0800

    added controller methods

--- ReviewApplication/ReviewApplication.API/Controllers/CommentController.cs
+++ ReviewApplication/ReviewApplication.API/Controllers/CommentController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper.QueryableExtensions;
+using AutoMapper;
 using ReviewApplication.Core.Domain;
 using ReviewApplication.Core.Infrastructure;
 using ReviewApplication.Core.Models;
@@ -9,6 +10,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+
+using System.Web.Http.Description;
+using System.Web.Http.OData;
 
 namespace ReviewApplication.API.Controllers
 {
@@ -16,7 +21,9 @@ namespace ReviewApplication.API.Controllers
     {
         private readonly ICommentRepository _commentRepository;
         private readonly IUnitOfWork _unitOfWork;
+        
 
+        //Constructor
         public CommentController(ICommentRepository commentRepository, IUnitOfWork unitOfWork)
         {
             _commentRepository = commentRepository;
@@ -24,36 +31,169 @@ namespace ReviewApplication.API.Controllers
         }
 
         // GET: api/Comment
+        [EnableQuery]
         public IQueryable<CommentModel> Get()
         {
             return _commentRepository.GetAll().ProjectTo<CommentModel>();
         }
 
         // GET: api/Comment/5
-        public string Get(int id)
+        [ResponseType(typeof(CommentModel))]
+        public IHttpActionResult GetComment(int id)
         {
-            return "value";
+            Comment dbComment = _commentRepository.GetByID(id);
+            if(dbComment == null)
+            {
+                return NotFound();
+            }
+            return Ok(Mapper.Map<CommentModel>(dbComment));
         }
 
-        // POST: api/Comment
-        public void Post([FromBody]string value)
-        {
-            _commentRepository.Add(new Core.Domain.Comment());
-
-            _unitOfWork.Commit();
-        }
 
         // PUT: api/Comment/5
-        public void Put(int id, [FromBody]string value)
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutComment(int id, CommentModel comment)
         {
-            
+            //Validate the request
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            if(id != comment.CommentID)
+            {
+                return BadRequest();
+            }
 
+            //Get the DB Comment  
+            var dbComment = _commentRepository.GetByID(id);
+            if(dbComment == null)
+            {
+                return NotFound();
+            }
+
+            //Update the Db Comment according to the input CommentModel object,
+            // and the update the comment in the DB
+            dbComment.Update(comment);
+            _commentRepository.Update(dbComment);
+
+            //Save Database Changes
+            try
+            {
+                _unitOfWork.Commit();
+            }
+            catch( DBConcurrencyException e)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw new Exception("Unable to update the comment in the database",e);
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // DELETE: api/Comment/5
-        public void Delete(int id)
+
+        // POST: api/Comment
+        [ResponseType(typeof(CommentModel))]
+        public IHttpActionResult PostCOmment(CommentModel comment)
         {
+          //Check ModelState
+          if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var dbComment = new Comment();
+
+            dbComment.Update(comment);
+
+            //add the new COmment object populated from input comment
+            _commentRepository.Add(dbComment);
+
+            //save the changes to the database
+            try
+            {
+                _unitOfWork.Commit();
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Unable to add comment to the database",e);
+           
+            }
+
+            //Return the created comment record
+            return CreatedAtRoute("DefaultApi", new { id = comment.CommentID }, comment);
+        }
+
+     
+       
+        // DELETE: api/Comment/5
+        [ResponseType(typeof(CommentModel))]
+        public IHttpActionResult DeleteComment(int id)
+        {
+            //Get the db COmment correspond to the comment ID
+            Comment dbComment = _commentRepository.GetByID(id);
+            if(dbComment == null)
+            {
+                return NotFound();
+            }
+            
+            //Delete Post
+            try
+            {
+                DeleteComments(id);
+
+                //Remove the comment
+                _commentRepository.Delete(dbComment);
+
+                //Save Changes
+                _unitOfWork.Commit();
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Unable to delete the comment from the database", e);
+            }
+
+            return Ok(Mapper.Map<CommentModel>(dbComment));
+        }
+
+
+
+
+
+
+
+
+        //--------------------------------------------
+        //Helper Methods
+
+        //Dispose
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        // Checks to see if Comment Exists
+        private bool CommentExists(int id)
+        {
+            return _commentRepository.Count(c => c.CommentID == id) > 0;
+        }
+
+        //Remove Comments corresponding to comment
+        private void DeleteComments (int commentID)
+        {
+            var dbComments = _commentRepository.Where(c => c.CommentID == commentID);
+
+            if(dbComments.Count() > 0)
+            {
+                foreach (var dbComment in dbComments)
+                {
+                    _commentRepository.Delete(dbComment);
+                }
+            }
         }
     }
 }
